2d9caa6e2587b1925e418ccfba7c2ab5ef0f0b8c
Lucas Martins <a.lucas460@gmail.com>
2018-09-30 21:31:20 -0300

    Create Project

---
+++ app/src/test/java/com/example/aluca/fshealth/alarme.java
@@ -0,0 +1,12 @@
+package com.example.aluca.fshealth;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class alarme extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        int time = etT
+    }
+}
---
+++ app/src/test/java/com/example/aluca/fshealth/remedio.java
@@ -0,0 +1,6 @@
+package com.example.aluca.fshealth;
+
+public class remedio {
+    String nome;
+}
+
