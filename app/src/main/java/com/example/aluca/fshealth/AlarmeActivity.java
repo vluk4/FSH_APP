2fab79fcafcab169b477b8e90920a96c283f2ed6
Victor Luca <vluca002@gmail.com>
2018-10-10 22:59:31 -0300

    Modificando layout.

--- app/src/main/java/com/example/aluca/fshealth/AlarmeActivity.java
+++ app/src/main/java/com/example/aluca/fshealth/AlarmeActivity.java
@@ -3,6 +3,9 @@ package com.example.aluca.fshealth;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class AlarmeActivity extends AppCompatActivity {
 
@@ -11,5 +14,22 @@ public class AlarmeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_alarm);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_alarme, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_formulario_ok:
+                Toast.makeText(AlarmeActivity.this, "Alarme Salvo!", Toast.LENGTH_SHORT).show();
+                finish();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
